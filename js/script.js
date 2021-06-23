69add70aa918b281db4bf37b35dfeaf4c98a7cbc
Roydano <roy.dano90@gmail.com>
2021-06-23 17:02:11 +0200

    add esercizio funzionante completato...ora modifico qualche caratteristica sl css

--- js/script.js
+++ js/script.js
@@ -13,6 +13,22 @@ const app = new Vue({
             'Rubare i soldi al vicino quando dorme',
             'Studiare per aumentare le skills'
         ]
+    },
+
+
+    methods: {
+
+        addInputInList(){
+            if(!this.inputToBo == ''){
+                this.toBoStart.push(this.inputToBo);
+            }
+            this.inputToBo = " ";
+        },
+
+        removeElementList(index){
+            this.toBoStart.splice(index, 1)
+
+        }
     }
 
 });
