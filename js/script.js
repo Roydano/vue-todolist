fc5b38ea0df40fdbbd5d0a1464bdfd8d4e7c9c1d
Roydano <roy.dano90@gmail.com>
2021-06-23 16:39:41 +0200

    abbozzo prime regole js

--- js/script.js
+++ js/script.js
@@ -3,4 +3,16 @@ const app = new Vue({
 
     el: '#app',
 
+    data: {
+
+        inputToBo: '',
+
+        toBoStart: [
+            'Fare la spesa',
+            'Portare il cane a passeggio',
+            'Rubare i soldi al vicino quando dorme',
+            'Studiare per aumentare le skills'
+        ]
+    }
+
 });
